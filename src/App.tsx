1d9e0d1b5d5b36df33604759375afb5c717bb953
EduardoR99 <102009964+EduardoR99@users.noreply.github.com>
2022-05-31 04:14:22 -0300

    first deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Item } from "./types/item";
+import { ListItem } from "./components/listItem";
+import * as C from "./app.styles";
+import { AddArea } from "./components/addArea";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Do the homework', done: false},
+    {id: 2, name: 'Clean the bedroom', done: true}
+  ]);
+
+  const handleAddTask = (taskName: string) =>{
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>
+          To Do List
+        </C.Header>
+
+        {/* New task add area */ }
+        <AddArea onEnter={handleAddTask}/>
+        {/* Task list */ }
+        {list.map((item, index) => (
+          <ListItem 
+          key={index} 
+          item={item}
+          onChange={handleTaskChange}
+          />
+        ))}
+      </C.Area>
+      <C.Footer>
+        Feito por Eduardo Rodrigues&copy; 
+      </C.Footer>
+    </C.Container>
+    
   );
 }
 
---
+++ src/components/addArea/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+    height: 30px;
+
+    .image{
+        margin-right: 6px;
+    }
+    input{
+        border: 0;
+        background: transparent;
+        outline: 0;
+        width: 100%;
+        color: #fff;
+        font-size: 18px;
+    }
+`;
